ae3db88719cb18546250113546b655b41f98de2f
bhavin.shah <bhavin.shah@einfochips.com>
2017-02-03 05:16:41 -0800

    Assignment 5

---
+++ src/common/common.module.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('common', [])
+    .constant('ApiPath', 'https://bhavin44-coursera5.herokuapp.com')
+    .config(config);
+
+    config.$inject = ['$httpProvider'];
+    function config($httpProvider) {
+        $httpProvider.interceptors.push('loadingHttpInterceptor');
+    }
+
+})();
---
+++ src/public/menu/menu.controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('public')
+    .controller('MenuController', MenuController);
+
+    MenuController.$inject = ['menuCategories'];
+    function MenuController(menuCategories) {
+        var $ctrl = this;
+        $ctrl.menuCategories = menuCategories;
+    }
+
+
+})();
